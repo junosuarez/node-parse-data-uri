fd1da186efb48fd1371f5a2e57431d02886a9563
jden <jason@denizac.org>
2014-08-26 02:17:26 -0400

    support messy mime types

--- test/test.js
+++ test/test.js
@@ -5,6 +5,13 @@ var test = require('fs').readFileSync(__dirname + '/test.datauri').toString()
 describe('parse-data-uri', function () {
   var parseDataUri = require('../')
   
+  it('works with crazy messed up mime types', function () {
+    var t = test.replace('image/gif','image/image/gif') 
+    // this is a test case i've seen from some datauris in the wild
+    parseDataUri(t).mimeType
+      .should.equal('image/gif')
+  })
+
   it('returns an object', function () {
 
     parseDataUri(test)
